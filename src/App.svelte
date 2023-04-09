a9979b73fb0af64fc0e6679d28e18ad92e86ab10
nanandr <rafirenan12@gmail.com>
2023-04-10 05:15:32 +0700

    styling 4/10

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,46 @@
 <script>
 	import {onMount} from "svelte";
 	import {musicList} from "./musiclist.js";
+	import Range from "Range.svelte";
 
 	let currentSong = 0;
-	let playerState = "play";
+	let playerState = false;
+	let timestamp = "duration";
 	let menu = "queue";
 	let audioElement;
+	let duration = 0;
+	let currentTime = 0;
 	let mainElement;
 
 	function prev(){
 		if(currentSong > 0){
 			currentSong--;
-			playerState = "play"
+			playerState = false
 		}
 	}
 	function playpause(){
-		if(playerState == "play"){
-			playerState = "pause";
+		if(playerState == false){
+			playerState = false;
 			audioElement.pause();
 		}
 		else{
-			playerState = "play"
+			playerState = false
 			audioElement.play();
 		}
+	}
+	function play(){
+		playerState = false;
+		audioElement.play();
 	}
 	function next(){
 		if(currentSong < $musicList.length-1){
 			currentSong++;
-			playerState = "play"
+			playerState = false
 		}
 	}
 	function setPlay(i){
 		currentSong = i;
-		playerState = "play";
+		playerState = false;
 	}
 	function showLyrics(){
 		menu = "lyrics";
@@ -40,11 +48,27 @@
 	function showQueue(){
 		menu = "queue";
 	}
+	function format(x){
+		const minute = Math.floor(x / 60);
+		const second = Math.floor(x % 60);
+		let format = minute + ":";
+		format += second < 10 ? "0" + second : second; 
+		return format;
+	}
+	function changeTimestamp(){
+		if(timestamp == "duration"){
+			timestamp = "timeLeft";
+		}
+		else{
+			timestamp = "duration";
+		}
+	}
 </script>
 
 <main bind:this={mainElement}>
 	<nav>
 		<h1>Sveltify</h1>
+		<div>Made by <a href="https://github.com/nanandr" target="_blank">nanandr</a></div>
 	</nav>
 	<div class="container">
 		<div class="player">
@@ -59,26 +83,41 @@
 					</div>
 					<div class="control">
 						<button on:click={prev}>
-							<i class="fa-backward fa-solid icon"></i>
+							<i class="fa-backward-step fa-solid icon"></i>
 						</button>
 						<button on:click={playpause}>
-							{#if playerState == "play"}
+							{#if playerState == false}
 								<i class="fa-pause fa-solid pause icon"></i>
+							{:else if playerState == true && currentTime == duration}
+								<i class="fa-rotate-left fa-solid replay icon"></i>
 							{:else}
 								<i class="fa-play fa-solid play icon"></i>
 							{/if}
 						</button>
 						<button on:click={next}>
-							<i class="fa-forward fa-solid icon"></i>
+							<i class="fa-forward-step fa-solid icon"></i>
 						</button>
 					</div>
-				</div>				
+				</div>
+				<div class="duration">
+					<input class="audio-control" type="range" bind:value={currentTime} max={duration} on:click={play}>	
+					<!-- svelte-ignore a11y-click-events-have-key-events -->
+					<p class="timestamp" on:click={changeTimestamp}>
+						{#if timestamp == "duration"}
+							{format(currentTime) + " / " + format(duration)}
+						{:else}
+							{format(currentTime) + " / -" + format(duration-currentTime)}
+						{/if}
+					</p>
+				</div>							
 				<audio
 					class="audio"
 					src={"./uploads/audio/" + $musicList[currentSong].audio}
 					bind:this={audioElement}
+					bind:paused={playerState}
+					bind:currentTime={currentTime}
+					bind:duration={duration}
 					autoplay="false"
-					controls
 				>
 				</audio>
 				{#if menu == "queue"}
